d547a8da424afabed2b40f819d255e337608735c
fortune cookie <767077147@qq.com>
2021-09-04 23:36:33 +0800

    chore: 更新cookie-module版本号

--- src/pages/rollup/index.tsx
+++ src/pages/rollup/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { TestComponent, Button } from 'cookie-react-component'
+import { Button } from 'cookie-react-component'
 
 const Rollup: React.FC = () => {
 	return (
 		<div>
 			<h1>tihs is rollup</h1>
-
-			<TestComponent theme='primary' />
-
-			<Button color='orange' tip='wowowo' />
+			<Button tip='haha' text='hahaha' color='red' />
 		</div>
 	)
 }
