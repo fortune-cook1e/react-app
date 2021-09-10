99db174e2ff11c687d35affe6caecf11d61c2224
fortune cookie <767077147@qq.com>
2021-09-11 00:14:07 +0800

    fix: gh-pages 限制 改为 hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useState, CSSProperties } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import { routes } from './routes'
 import Error from './components/ErrorBoundary'
