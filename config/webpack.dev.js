f356c6c7b286c1ecba05b438ae010e33d884f4a3
fortune cookie <767077147@qq.com>
2022-03-15 19:13:38 +0800

    build: 更新react热更新

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,24 +14,24 @@ module.exports = merge(commonConfig, {
 		compress: true,
 		hot: true,
 		port: 8081
-	}
+	},
 
-	// module: {
-	// 	rules: [
-	// 		{
-	// 			test: jstsRegex,
-	// 			exclude: /node_modules/,
-	// 			use: [
-	// 				{
-	// 					loader: 'babel-loader',
-	// 					options: {
-	// 						cacheDirectory: true,
-	// 						plugins: [require.resolve('react-refresh/babel')].filter(Boolean)
-	// 					}
-	// 				}
-	// 			]
-	// 		}
-	// 	]
-	// },
-	// plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean)
+	module: {
+		rules: [
+			{
+				test: jstsRegex,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: true,
+							plugins: [require.resolve('react-refresh/babel')].filter(Boolean)
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean)
 })
