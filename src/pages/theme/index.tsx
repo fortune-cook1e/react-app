754e6f1f1e2166b3a74d4771e7e32921eb1a53d7
fortune cookie <767077147@qq.com>
2022-04-05 17:50:02 +0800

    feat: 原生css3 修改主题功能完成

--- src/pages/theme/index.tsx
+++ src/pages/theme/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'antd'
 import { ConfigProvider } from 'antd'
+import styles from './index.module.less'
 
 const COLORS = ['#25b864', '#f65c2d', '#faad14']
 let INDEX = 0
@@ -17,12 +18,24 @@ const ThemePage = (): JSX.Element => {
 		})
 	}
 
+	const handleCss3Click = () => {
+		document.documentElement.style.setProperty('--primary-color', '#f65c2d')
+	}
+
 	return (
 		<div>
-			this is theme pages
-			<Button type='primary' onClick={handleClick}>
-				click me
-			</Button>
+			<section>
+				<h3>antd 主题测试</h3>
+				this is theme pages
+				<Button type='primary' onClick={handleClick}>
+					click me
+				</Button>
+			</section>
+
+			<section>
+				<h3 className={styles.variable__text}>css3 原生颜色主题测试</h3>
+				<Button onClick={handleCss3Click}>click me</Button>
+			</section>
 		</div>
 	)
 }
