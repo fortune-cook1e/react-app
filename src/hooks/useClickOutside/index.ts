5491c8c37b7b4af0159cbea6461e4cf780047e8a
fortune cookie <767077147@qq.com>
2022-04-10 10:26:33 +0800

    fix: 调整useClickOutside hooks参数类型

--- src/hooks/useClickOutside/index.ts
+++ src/hooks/useClickOutside/index.ts
@@ -1,6 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, MutableRefObject } from 'react'
 
-const useClickOutside = (eleRef: any, callback: (e: Event) => void): void => {
+const useClickOutside = (
+	eleRef: MutableRefObject<any>,
+	callback: (e: Event) => void
+): void => {
 	useEffect(() => {
 		const handleClickOutside = (e: Event) => {
 			if (!eleRef.current.contains(e.target) && callback) {
