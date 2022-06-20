c9f5523636ebad0bc12a88698f52294235cc1aff
fortune-cook1e <767077147@qq.com>
2022-06-20 16:24:35 +0000

    Deploying to gh-pages from @ fortune-cook1e/react-app@296083132d0a526fe013cd882936ad0180734d40 🚀

---
+++ js/748.509e0396ce7740afefc4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[748],{6748:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var u=t(80),c=t(7294),r=t(5893),s=function(){return(0,c.useEffect)((function(){(0,u.Rf)().then((function(e){console.log({res:e})}))}),[]),(0,r.jsx)("div",{children:"this is user management page"})}}}]);
