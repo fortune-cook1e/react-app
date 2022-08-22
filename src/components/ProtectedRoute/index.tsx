8897c8980b06cfdbe6fad138ee87b968c37240ed
fortune cookie <767077147@qq.com>
2022-08-22 22:29:27 +0800

    refactor: 关闭路由拦截

--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -14,12 +14,11 @@ const ProtectedRoute = ({ children }: Props): JSX.Element => {
 	const queryClient = useQueryClient()
 
 	useEffect(() => {
-		// TIP: 当用户没有登录时 跳转到登录页 并使所有请求缓存失效
-		// TIP: 只有在开发环境下 路由拦截才生效 生效根据env文件的VITE_APP_ENV字段决定
-		if (!isUserLogin && globalConfig.isDev) {
-			navigate('/login')
-			queryClient.invalidateQueries()
-		}
+		// TIP: 目前不开启路由拦截
+		// if (!isUserLogin && globalConfig.isDev) {
+		// 	navigate('/login')
+		// 	queryClient.invalidateQueries()
+		// }
 	}, [])
 	return <>{children}</>
 }
