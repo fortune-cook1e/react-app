f4ca342c1e6e491c39806e0e6d1d01d028761953
fortune cookie <767077147@qq.com>
2022-09-24 18:40:58 +0800

    style: 删除 react-query 相关内容

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -5,26 +5,13 @@ import { ConfigProvider } from 'antd'
 import zhCN from 'antd/lib/locale/zh_CN'
 import store from '@/store'
 import App from './App'
-import { QueryClient, QueryClientProvider } from 'react-query'
 import './styles/base.less'
-
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false, // 窗口聚焦不重新获取数据
-			retry: 1, // 失败只会重试一次
-			retryDelay: 1000 // 重试等待时间
-		}
-	}
-})
 
 render(
 	<ConfigProvider locale={zhCN}>
-		<QueryClientProvider client={queryClient}>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</QueryClientProvider>
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</ConfigProvider>,
 
 	document.getElementById('app')
