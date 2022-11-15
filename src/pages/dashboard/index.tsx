d2b01a8464d9bce047d528e326fb8a840c96ca0f
fortune_cookie <767077147@qq.com>
2022-11-15 18:12:49 +0800

    feat: 增加dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Button } from 'antd'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = (): JSX.Element => {
+  const [state1, setState1] = useState<number>(0)
+  const [state2, setState2] = useState<number>(0)
+
+  useEffect(() => {
+    console.log('changed..')
+  }, [state1, state2])
+
+  const changeState = () => {
+    setState1(2)
+    setState2(2)
+  }
+
   return (
     <div>
+      <Button onClick={changeState}>click m</Button>
       <h1>this is dashboard page</h1>
     </div>
   )
