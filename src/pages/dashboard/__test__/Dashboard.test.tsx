25fd086040ab26741be653fc17fb9d0c80812aa6
fortune_cookie <767077147@qq.com>
2022-11-28 17:55:14 +0800

    chore: 增加 jest

---
+++ src/pages/dashboard/__test__/Dashboard.test.tsx
@@ -0,0 +1,8 @@
+import { cleanup, fireEvent, render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
+
+import Dashboard from '..'
+
+describe('<Dashboard />', () => {
+  it('test text render', () => {})
+})
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,18 @@
 import { Button } from 'antd'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Dashboard = (): JSX.Element => {
-  const [state1, setState1] = useState<number>(0)
-  const [state2, setState2] = useState<number>(0)
-
-  useEffect(() => {
-    console.log('changed..')
-  }, [state1, state2])
+  const [text, setText] = useState<string>('text')
 
   const changeState = () => {
-    setState1(2)
-    setState2(2)
+    setText('text222')
   }
 
   return (
     <div>
       <Button onClick={changeState}>click m</Button>
       <h1>this is dashboard page</h1>
+      <h2>{text}</h2>
     </div>
   )
 }
